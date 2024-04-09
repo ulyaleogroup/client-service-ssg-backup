b504873110722e2dc3d7ff1bfef0b7bdb05d7ed5
ulyapebriyana <ulyapebriyanaalisaputra@gmail.com>
2024-04-09 21:37:14 +0700

    add memberst

--- components/member-detail.tsx
+++ components/member-detail.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Table,
   TableBody,
@@ -10,14 +12,90 @@ import {
 } from "@/components/ui/table";
 import { getMemberDetail } from "@/lib/admin-actions";
 import moment from "moment";
+import React, { useState } from "react";
+import useSWR from "swr";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-const MemberDetail = async () => {
-  const members = await getMemberDetail();
+const fetcher = async (...args: Parameters<typeof fetch>) => {
+  const res = await fetch(...args);
+  return res.json();
+};
+
+const MemberDetail =  () => {
+
+  const [page, setPage] = useState(1);
+
+  const { data, error } = useSWR(
+    `${window.location.origin}/api/members?page=${page}`,
+    fetcher
+  );
+
+  const members = data?.data;
+  const totalPages = data?.metadata?.totalPages || 0;
+
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setPage((prevPage) => prevPage - 1);
+  };
+
+  const goToPage = (pageNumber: any) => {
+    setPage(pageNumber);
+  };
+
+  const renderPageNumbers = () => {
+    const range = 2; // Jumlah halaman yang akan ditampilkan sebelum dan sesudah halaman saat ini
+    const currentPage = page;
+  
+    const start = Math.max(1, currentPage - range);
+    const end = Math.min(totalPages, currentPage + range);
+  
+    const pageNumbers = [];
+  
+    if (start > 1) {
+      pageNumbers.push(
+        <button key={1} onClick={() => goToPage(1)} className="text-lg p-2">
+          1
+        </button>
+      );
+  
+      if (start > 2) {
+        pageNumbers.push(
+          <span key="start-ellipsis" className="p-2">...</span>
+        );
+      }
+    }
+  
+    for (let i = start; i <= end; i++) {
+      pageNumbers.push(
+        <button key={i} onClick={() => goToPage(i)} disabled={i === page} className={`text-lg p-2 ${i === currentPage ? 'text-primary' : ''}`}>
+          {i}
+        </button>
+      );
+    }
+  
+    if (end < totalPages) {
+      if (end < totalPages - 1) {
+        pageNumbers.push(
+          <span key="end-ellipsis" className="p-2">...</span>
+        );
+      }
+  
+      pageNumbers.push(
+        <button key={totalPages} onClick={() => goToPage(totalPages)} className="text-lg p-2">
+          {totalPages}
+        </button>
+      );
+    }
+  
+    return pageNumbers;
+  };
 
   return (
     <div className="rounded-md border">
       <Table>
-        <TableCaption>A list of your recent membership.</TableCaption>
         <TableHeader className="bg-muted/50">
           <TableRow>
             <TableHead className="">Name</TableHead>
@@ -27,7 +105,7 @@ const MemberDetail = async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {members.map((member) => (
+          {members?.map((member: any) => (
             <TableRow key={member.id}>
               <TableCell className="font-medium">
                 {member.name}
@@ -43,9 +121,28 @@ const MemberDetail = async () => {
           ))}
         </TableBody>
         <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
+        <TableRow className="text-end">
+            <TableCell colSpan={7}>
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={prevPage}
+                  disabled={data?.metadata?.currentPage < 2}
+                  className="border rounded-sm p-2"
+                >
+                  <ChevronLeft />
+                </button>
+                {renderPageNumbers()}
+                <button
+                  onClick={nextPage}
+                  disabled={
+                    data?.metadata?.currentPage > data?.metadata?.totalPages - 1
+                  }
+                  className="border rounded-sm p-2"
+                >
+                  <ChevronRight />
+                </button>
+              </div>
+            </TableCell>
           </TableRow>
         </TableFooter>
       </Table>
